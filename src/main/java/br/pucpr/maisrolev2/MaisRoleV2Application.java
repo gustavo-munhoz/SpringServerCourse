0f13ef4508839c441ac149272c2f01bb2436f7d5
andre <andre.wozniack@pucpr.edu.br>
2023-02-11 13:06:35 -0300

    Init

---
+++ src/main/java/br/pucpr/maisrolev2/MaisRoleV2Application.java
@@ -0,0 +1,13 @@
+package br.pucpr.maisrolev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisRoleV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaisRoleV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/pucpr/maisrolev2/MaisRoleV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucpr.maisrolev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisRoleV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
