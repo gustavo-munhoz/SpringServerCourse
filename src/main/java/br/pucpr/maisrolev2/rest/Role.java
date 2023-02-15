7b25b5e2a2c4fb109badeed91f6ced782637559f
gustavo-munhoz <gustavo.munhoz015@gmail.com>
2023-02-15 20:19:36 -0300

    added roles

--- src/main/java/br/pucpr/maisrolev2/rest/Role.java
+++ src/main/java/br/pucpr/maisrolev2/rest/Role.java
@@ -1,13 +1,5 @@
 package br.pucpr.maisrolev2.rest;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
-public class Role {
-    @Id
-    @GeneratedValue
-    private Long id;
-    @Column(unique = true)
-    private String name;
+public enum Role {
+    ADMIN, USER, HOST
 }
